70a309b0759a68e92b011bd97ea9ce6cbe0844b2
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-04-08 21:51:23 +0300

    generated main skeleton of site on App.scss ,some css styling, removed some excel.css fonts, updated links to components from App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 import SkillsViewer from './modules/SkillsViewer/SkillsViewer';
+import Resume from './modules/Resume/Resume';
+import ResumeGit from './modules/ResumeGit/ResumeGit';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SkillsViewer></SkillsViewer>
+        <Resume /> <ResumeGit />
       </header>
+      <SkillsViewer></SkillsViewer>
     </div>
   );
 }
