3bfbac66594a1facbd03c55c71d115dcbbe4bc13
frontendLearning-31082022 <MorglHer@yandex.ru>
2024-04-11 22:03:08 +0300

    WordCarousel converted to component

--- src/canvas/word_carousel/WordCarousel.js
+++ src/canvas/word_carousel/WordCarousel.js
@@ -1,36 +1,45 @@
 import React from 'react'
-import { useEffect, useState } from 'react';
 
-import './WordCarousel.scss'
+const style = {
+    keyframe: {
+        one: '-50%',
+        two: '- 5px',
+        speed: '90s'
+    },
+    container: {
+        mask: 'linear-gradient(90deg, transparent, white 40%, white 60%, transparent)',
+        '-webkit-mask': 'linear-gradient(90deg, transparent, white 40%, white 60%, transparent)',
+        overflow: 'hidden',
+        width: '100%',
+    },
+    ul: {
+        display: 'flex',
+        gap: '10px',
+        'list-style': 'none',
+        padding: '0',
+        width: 'max-content',
+    },
+}
 
 export default function WordCarousel(words) {
-
-    useEffect(() => {
-        const list = document.querySelector("#list");
-        const listContent = Array.from(list.children);
-        
-        listContent.forEach(item => {
-            const duplicatedItem = item.cloneNode(true);
-            duplicatedItem.setAttribute("aria-hidden",true);
-            list.appendChild(duplicatedItem);
-        });
-    }, []);
+    const c = words.onClickElement;
 
     return (
         <div>
-            <div id="container" data-animated>
-                <ul id="list">
-                    <li>First</li>
-                    <li>Second</li>
-                    <li>Third</li>
-                    <li>Fourth</li>
-                    <li>Fifth</li>
-                    <li>First</li>
-                    <li>Second</li>
-                    <li>Third</li>
-                    <li>Fourth</li>
-                    <li>Fifth</li>
-                </ul> </div>
+            <style>{`@keyframes scroll {
+             to {
+              translate: calc(${style.keyframe.one} ${style.keyframe.two}); }}
+
+               .w_carousel_container .w_carousel_list {
+                  animation: scroll ${style.keyframe.speed} linear infinite; } `}</style>
+
+            <div className="w_carousel_container" style={style.container} data-animated>
+                <ul className="w_carousel_list" style={style.ul}>
+                    {words.words.map((t) => <li onClick={(e) => { c(e) }}>{t}</li>)}
+                    {words.words.map((t) => <li aria-hidden="true" onClick={(e) => { c(e) }}>{t}</li>)}
+                    {words.words.map((t) => <li aria-hidden="true" onClick={(e) => { c(e) }}>{t}</li>)}
+                </ul>
+            </div>
         </div>
     )
 }
