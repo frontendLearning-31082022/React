46eb4fa08d8b6c5e198599177d675b5d54d112a7
frontendLearning-31082022 <MorglHer@yandex.ru>
2025-01-28 16:17:51 +0300

    prepare lightweight list of words

--- src/modules/WordsList.js
+++ src/modules/WordsList.js
@@ -3,16 +3,9 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useStore } from 'react-redux'
 // import { incr } from './../app/store'
 import { useWordsQuery, wordsApi } from 'features/api/apiSlice';
-
+import "./../css/WordList.scss"
 
 export default function WordsList() {
-    // const store = useStore();
-    // const q = apiSlice.useGetPostsQuery();
-
-    // const [words, setWords] = useState([]);
-
-    // const { useGetPostsQuery } = apiSlice;
-    // debugger;
 
     const { data, error, isLoading, isFetching, isSuccess } = useWordsQuery(
         { offset: 0 }
@@ -22,26 +15,21 @@ export default function WordsList() {
 
     return (
         <div>
-
-            {/* <button onClick={() => { loadWords(); }}>Подгрузить</button> */}
-            {/* <button onClick={() => { ds(incr()) }}>Тест</button> */}
-
-
             <div>{
                 (isSuccess) ?
                     data?.map((d) => {
                         // debugger;
                         return (
-                            <div className="data">{d.word_rus}</div>
+                            <div className='word'>
+                                <div className="word__rus">{d.word_rus}</div>
+                                <div className="word__eng">{d.word_eng}</div>
+                                <div className="word__meaning">{d.meaning}</div>
+                            </div>
                         );
                     })
                     : "Loading..."
 
             }</div>
-
-            {/* <div>
-                {words.map((t,i) => <div key={i}>{t.word_rus}</div>)}
-            </div> */}
 
 
         </div>
