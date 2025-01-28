8a8d58277f7f1351919f47a7fc98719b3b28a498
frontendLearning-31082022 <MorglHer@yandex.ru>
2025-01-28 16:14:37 +0300

    prepare tags for cache, get new url useCompleteWordStatusQuery

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,22 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-//, fetchFn: fetch
 
-// queryFn: (arg, _api, _extraOptions, fetchWithBQ) => {
-//     // debugger;
-//     return {data: {} };
-// }
   export const wordsApi = createApi({
     reducerPath: "missionsApi",
     baseQuery: fetchBaseQuery({
       baseUrl: process.env.REACT_APP_API,
       mode: "cors"
     }),
+    tagTypes:["completeWord"],
     endpoints: (builder) => ({
       words: builder.query({
         // query: () =>  process.env.REACT_APP_Words_getNoLearned,
         query: ({offset}) =>  process.env.REACT_APP_Words_getNoLearned+`?offset=${offset}`,
       }),
+      completeWordStatus:builder.query({
+        providesTags: ['completeWord'],
+        query: ({id}) =>  process.env.REACT_APP_Words_complete_word_status+`?idWord=${id}`,
+      }),
     })
   });
   
-  export const { useWordsQuery } = wordsApi;
+  export const { useWordsQuery,useCompleteWordStatusQuery } = wordsApi;
