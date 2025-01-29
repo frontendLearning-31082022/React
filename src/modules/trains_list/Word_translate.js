cdd038c88fb39b72b74bddb5530a8ef04288b1ce
frontendLearning-31082022 <MorglHer@yandex.ru>
2025-01-30 00:15:21 +0300

    continue prepare word-translate train

--- src/modules/trains_list/Word_translate.js
+++ src/modules/trains_list/Word_translate.js
@@ -5,14 +5,17 @@ import React, { useEffect, useState } from 'react'
 import { useWordsQuery, useCompleteWordStatusQuery, wordsApi } from 'features/api/apiSlice';
 import "./../../css/Word_translate.scss"
 import { useDispatch } from 'react-redux';
-import {increment} from '../../app/store';
-import {store} from '../../app/store';
+import { increment } from '../../app/store';
+import { store } from '../../app/store';
 
 export default function Word_translate() {
   const [currentWord, setCurrentWord] = useState(null);
-  const [wordsLocal, setWordsLocal] = useState([]);
+  let [wordsLocal, setWordsLocal] = useState([]);
 
-  const { data,currentData, error, isLoading, isFetching, isSuccess } = useWordsQuery(
+  const [showResolve, setShowResolve] = useState([false]);
+  const [markWrongAnwer, setMarkWrongAnwer] = useState([false]);
+
+  const { data, currentData, error, isLoading, isFetching, isSuccess } = useWordsQuery(
     { offset: 0 }
   );
 
@@ -24,45 +27,61 @@ export default function Word_translate() {
     return data;
   }
 
-  const rndWordSwitch = (e) => {
-    const numWord = Math.floor(Math.random() * (wordsLocal.length - 0 + 1)) + 0;
-    const word = wordsLocal[numWord];
-    setCurrentWord(word);
-  }
+
+  // const rndWordSwitch = (e) => {
+
+  // }
 
   const saveWordsToLocal = async () => {
     if (wordsLocal.length > 0) return;
-    if(data)setWordsLocal(data);
+    if (data) setWordsLocal(data);
   }
 
   const checkAnswer = () => {
     const answerVal = document.getElementById("answer").value;
     const wrong = answerVal != currentWord.word_rus;
 
-    debugger;
+    setShowResolve(false);
+    const objPercent = { word: currentWord.word_eng, wrong: wrong };
+    store.dispatch(increment(objPercent));
+
+    const wordRemove = wordsLocal.indexOf(currentWord);
+    wordsLocal = wordsLocal.filter((x, i) => wordRemove != i);
+    setWordsLocal(wordsLocal);
+  }
+  const nextWordBtn = () => {
+    setShowResolve(true);
+    setCurrentWord(null);
   }
 
   useEffect(() => {
     saveWordsToLocal();
-    store.dispatch(increment());
+
   }, [data]);
+
+  useEffect(() => {
+    if (currentWord != null) return;
+    const numWord = Math.floor(Math.random() * (wordsLocal.length - 0 + 1)) + 0;
+    const word = wordsLocal[numWord];
+    setCurrentWord(word);
+
+  }, [currentWord]);
 
   return (
     <div>
       Тренировка АнглСлово-Перевод
-      {currentWord == null ? <button onClick={rndWordSwitch}>Начать "Слово-Перевод"</button> : ""}
+      {/* {currentWord == null ? <button onClick={rndWordSwitch}>Начать "Слово-Перевод"</button> : ""} */}
 
       {currentWord == null ? "" :
         <div className='cards_container'>
           <div className='cards_words'>
             <div className="WordAtEng card">{currentWord.word_eng}</div>
-            <div className="WordAtRus card card-hided">{currentWord.word_rus}</div>
+            <div className={"WordAtRus card " + (showResolve ? "card-hided" : "")} id="resolve">{currentWord.word_rus}</div>
           </div>
           <div className='answer_panel'>
-            <input id='answer' onKeyDown={(e)=>{ if(e.keyCode == 13)checkAnswer()} }  
-// @ts-ignore
-            autocomplete="off"/>
+            <input id='answer' onKeyDown={(e) => { if (e.keyCode == 13) checkAnswer() }} autoComplete="off" />
             <button onClick={checkAnswer} >Проверить</button>
+            <button onClick={nextWordBtn} >Дальше</button>
           </div>
         </div>
 
